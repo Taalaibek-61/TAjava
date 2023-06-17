ac8b4f9845f3b5de27d5f223cab4e3241546fd54
Taalaibek <abatelosh@mail.ru>
2023-06-17 07:15:58 +0600

    задача2 семинар2

--- Exes_2_2.java
+++ Exes_2_2.java
@@ -1,2 +1,33 @@
-package org.example.Seminar1.HomeWork;public class Exes_2_2 {
+package org.example.Seminar1.HomeWork;
+
+import java.util.Scanner;
+
+public class Exes_2_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите кол-во чисел: ");
+        int n = scanner.nextInt();
+        System.out.println("zadanie_2(n,scanner) = " + zadanie_2(n, scanner));
+    }
+
+    /**+
+     *  Дана последовательность из N целых чисел. Верно ли, что последовательность является возрастающей.
+     * @param n
+     * @param scanner
+     * @return
+     */
+
+    private static boolean zadanie_2(int n, Scanner scanner) {
+        int count = 0;
+        int a = 0, b = 0;
+        a = scanner.nextInt();
+        for (int i = 0; i < n-1 ; i++) {
+            b = scanner.nextInt();
+            if(a > b) return false;
+
+
+
+        }
+        return true;
+    }
 }
