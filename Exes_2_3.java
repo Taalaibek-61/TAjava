bc5a5733558f158583d36a7a2734d1780acdfd2e
Taalaibek <abatelosh@mail.ru>
2023-06-17 07:17:25 +0600

    задача3 семинар2

--- Exes_2_3.java
+++ Exes_2_3.java
@@ -1,2 +1,31 @@
-package org.example.Seminar1.HomeWork;public class Exes_2_3 {
+package org.example.Seminar1.HomeWork;
+
+public class Exes_2_3 {
+    public static void main(String[] args) {
+        int[] array = {1, 22, 33, 44, 5, -5, -3, 23, -7};
+        System.out.println("summInd(array) = " + summInd(array));
+    }
+
+    /**+
+     * Дан массив целых чисел. Заменить отрицательные элементы на сумму индексов двузначных элементов массива.
+     * @param array массив
+     * @return массив
+     */
+    private static int[] summInd(int[] array) {
+                 sum = 0;
+              result = 0;
+      int[] result = int[ array.length];
+        for (int i = 0; i < array.length; i++) {
+            if(10 =< array[i] && array[i] < 100 || -10 =< array[i] && array[i] > -100) sum+=i;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] < 0){
+                    array[i] = sum;
+                }
+              int[] result =  array[i];
+                }
+        return int[]result;
+            }
+
+        }
+    }
 }
